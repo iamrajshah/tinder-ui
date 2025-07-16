b147b6f0a02d688235cb46b1a06fc2055b88d67c
iamrajshah <shahrajesh2113@yahoo.com>
2025-07-16 13:52:53 +0530

    Link added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,40 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
-
+import axios from "axios"
+import {BASE_URL} from './../utils/constants'
+import { useDispatch, useSelector } from "react-redux"
+import {addUser} from './../utils/userSlice'
+import { useState } from "react"
 function Body() {
+    const dispatch = useDispatch();   
+    const navigate = useNavigate(); 
+
+    const user = useSelector((store) => store.user)
+
+    const fetchUser  = async() => {
+        try {
+            if (user) return
+            const userData = await axios.get(BASE_URL + '/profile/view', {
+                withCredentials: true
+            })
+            console.log(userData);
+           
+            dispatch(addUser(userData.data))
+        } catch (error) {
+            console.log(error);
+            if (error.status === 401) 
+                navigate('/login')
+        }
+    }
+
+    useState(()=> {
+        
+        console.log(user)
+
+        if (!user)
+            fetchUser()
+    }, [])
 return (
     <>
        <NavBar />
