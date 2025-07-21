423b98ec9e50d58b747d4e192867734f7c351bd0
iamrajshah <shahrajesh2113@yahoo.com>
2025-07-21 19:10:16 +0530

    Added web-socket

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addConnections } from "../utils/connectionSlice";
+import { Link } from "react-router-dom";
 
 const Connections = () => {
   const dispatch = useDispatch();
@@ -13,7 +14,6 @@ const Connections = () => {
       const connections = await axios.get(BASE_URL + "/user/connections", {
         withCredentials: true,
       });
-      console.log(connections);
       dispatch(addConnections(connections.data.data));
     } catch (error) {
       setError(error?.data?.message);
@@ -36,7 +36,8 @@ const Connections = () => {
     <div className="text-center my-10">
       <h1 className="text-bold text-3xl">Connections</h1>
       {allConnections.map((connect) => {
-        const { firstName, lastName, photoUrl, age, gender, about } = connect;
+        const { _id, firstName, lastName, photoUrl, age, gender, about } =
+          connect;
         return (
           <div className="flex m-4 p-4 rounded-lg w-200 mx-auto bg-gray-300">
             <div>
@@ -53,6 +54,9 @@ const Connections = () => {
               {age && gender && <p>{age + ",  " + gender}</p>}
               <h3 className="text-sm ">{about}</h3>
             </div>
+            <Link to={"/chat/" + _id}>
+              <button className="btn btn-secondary">Chat</button>
+            </Link>
           </div>
         );
       })}
